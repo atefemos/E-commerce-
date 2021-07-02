97b6682082026fe3801c73d7c806a37ebb5e28e1
Atefe Moslemi <a.moslemi.68gmail.com>
2021-07-02 08:52:38 +0430

    make login page basic & rtl project

--- final/src/App.js
+++ final/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.scss";
+import { create } from "jss";
+import rtl from "jss-rtl";
+import { StylesProvider, jssPreset } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProductsList from "./pages/customer/ProductsList";
 import Cards from "./pages/customer/Cards";
@@ -7,20 +10,30 @@ import Profile from "./pages/customer/Profile";
 import NotFound from "./pages/NotFound";
 import LogIn from "./pages/admin/LogIn";
 import Panel from "./pages/admin/Panel";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const theme = createMuiTheme({
+  direction: "rtl",
+});
 
 function App() {
   return (
     <React.Fragment className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={ProductsList} />
-          <Route path="/cards" exact component={Cards} />
-          <Route path="/profile" exact component={Profile} />
-          <Route path="/login" exact component={LogIn} />
-          <Route path="/login/panel" exact component={Panel} />
-          <Route path="*" exact component={NotFound} />
-        </Switch>
-      </Router>
+      <StylesProvider jss={jss}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={ProductsList} />
+              <Route path="/cards" exact component={Cards} />
+              <Route path="/profile" exact component={Profile} />
+              <Route path="/login" exact component={LogIn} />
+              <Route path="/login/panel" exact component={Panel} />
+              <Route path="*" exact component={NotFound} />
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </StylesProvider>
     </React.Fragment>
   );
 }
--- final/src/pages/admin/LogIn.jsx
+++ final/src/pages/admin/LogIn.jsx
@@ -1,7 +1,121 @@
 import React from "react";
+import "../../assets/styles.scss";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const LogIn = () => {
-  return <div>Logggggg innnnn</div>;
-};
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Paliz Store
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-export default LogIn;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(6),
+    color: "rgba(49,69,106,0.35)",
+
+    boxShadow:
+      "6px 6px 10px -1px rgba(0,0,0,0.15),-6px -6px 10px -1px rgba(255,255,255,0.7)",
+    borderRadius: "15px",
+    border: "1px solid rgba(0,0,0,0.0)",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          ورود
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="ایمیل یا نام کاربری"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="رمز عبور"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            ورود
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                رمز عبور خود را فراموش کرده ام{" "}
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/" variant="body2">
+                بازگشت به صفحه اصلی
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
