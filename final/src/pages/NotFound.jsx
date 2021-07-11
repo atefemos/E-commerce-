ee80e64a6e700e6953ed5b1d9035196283b3e589
Atena Moslemi <a.moslemi.68@gmail.com>
2021-07-12 00:01:24 +0430

    create not found page

--- final/src/pages/NotFound.jsx
+++ final/src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Btn from "../components/Btn";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Not found</div>;
+  return (
+    <div className="root fullVH">
+      <div className="box">
+        <h3>صفحه مورد نظر شما پیدا نشد.</h3>
+        <p>لطفا به پالیز استور برگردید ...</p>
+        <Link to={"/"} className="link">
+          <Btn text={"بازگشت به پالیز استور"} color={"secondary"} />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
