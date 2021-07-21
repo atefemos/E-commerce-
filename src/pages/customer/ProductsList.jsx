07e247778137ae98f15dcd3cce7dd7e921be014c
Atefe Moslemi <a.moslemi.68gmail.com>
2021-07-21 13:13:20 +0430

    make mainHeader component

--- src/pages/customer/ProductsList.jsx
+++ src/pages/customer/ProductsList.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import MainHeader from "../../components/MainHeader";
 
 const ProductsList = () => {
   return (
-    <div>kld</div>
+    <div>
+      <MainHeader />
+    </div>
   );
 };
 
